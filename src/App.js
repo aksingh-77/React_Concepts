969d4dca58757035e5a549608c0a6ad4d0306459
Abhishek Singh <aksinghpro77@gmail.com>
2023-12-14 17:46:07 +0530

    JSX Alternate Method React.createElement()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const App = () => {
+  // return (
+  //   <div>
+  //     Hello world!!
+  //   </div>
+  // );
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  //------------------------------------IMP Points---------------------------------------//
+  // This is method we can use in alternative to JSX,
+
+  // React.createElement takes atleast of 3 parameters
+  // 1st ==> Element which we want to create in quotes
+  // 2nd ==> Attributes and its vlaue in JSON FormData, as we dont have any we have passed empty curly BroadcastChannel
+  // 3rd ==> Its Child element w/o quotes
+  return (React.createElement("div", {}, React.createElement("h1", {}, "Hello World")));
+
+  /**
+   * WE can also use createElement method inside an another createElement method
+   * eg React.createElement("div", {}, React.createElement("h1", {}, "Hello World"))
+   */
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const element = <h1>Hello world!!...</h1>;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<App />
 );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
